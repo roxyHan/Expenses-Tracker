f345b4fa186d8b2bbd31f98ee516bbacf73b5f01
roxyHan <40450542+roxyHan@users.noreply.github.com>
2024-10-28 22:07:17 -0400

    Exit condition

--- user_interaction.py
+++ user_interaction.py
@@ -1,28 +1,31 @@
 import manage_expenses as man
 
 def main():
-    menu()
-    prompt = int(input("\nPlease select your option:\n"))
+    running = True
     
-    if prompt == 1:
-        man.add_expenses()
-    elif prompt == 2:
-        man.view_expenses()
-    elif prompt == 3:
-        man.category_summary()
-    elif prompt == 4:
-        man.get_status()
-    elif prompt == 5:
-        man.spending_trend()
-    else:
-        print("Please select one of the options listed: 1 through 5\n")
+    while (running):
+        menu()
+        prompt = int(input("\nPlease select your option:\n")) 
+        if prompt == 1:
+            man.add_expenses()
+        elif prompt == 2:
+            man.view_expenses()
+        elif prompt == 3:
+            man.category_summary()
+        elif prompt == 4:
+            man.get_status()
+        elif prompt == 5:
+            man.spending_trend()
+        elif prompt == 6:
+            running = False
+            print("Thank you. Enjoy!")
+        else:
+            print("Please select one of the options listed: 1 through 5\n")
 
 
 def menu():
     print("Welcome to Track Expert:\n", \
-        "[1] Add an expense\n  [2] View expenses\n [3] View categories\n  [4] Get Status\n  [5] Display Trends\n [6] Other")
-
-
+        "[1] Add an expense\n  [2] View expenses\n [3] View categories\n  [4] Get Status\n  [5] Display Trends\n [6] Leave")
 
 
 if __name__ == "__main__":
