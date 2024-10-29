40f418994fbe77f6c2a8ea451a0cfc240aa9a763
roxyHan <40450542+roxyHan@users.noreply.github.com>
2024-10-28 21:52:00 -0400

    Basic structure of the functions

---
+++ manage_expenses.py
@@ -0,0 +1,37 @@
+'''
+[1]: Users can add an expense by mentionning the amount, category
+
+'''
+def add_expenses():
+    return None
+
+'''
+[2]: View expenses
+'''
+def view_expenses():
+    return None
+
+'''
+[3] : Get status 
+'''
+def get_status():
+    return balance
+
+'''
+[4] : Category summary 
+'''
+def category_summary():
+    return None
+
+''' 
+[5] : Spending trends
+'''
+def spending_trend():
+    return None
+
+
+''' 
+[6] : Send us a message
+'''
+def send_message():
+    return ""
